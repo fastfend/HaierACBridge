a9360815a41ed7549ba87fbec53151b80c70ca56
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-06-25 19:46:01 +0200

    Attempt to fix App has tendency to close in background #1

--- app/src/main/java/com/fastfend/haieracbridge/BackgroundService.java
+++ app/src/main/java/com/fastfend/haieracbridge/BackgroundService.java
@@ -1,17 +1,20 @@
 package com.fastfend.haieracbridge;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import com.fastfend.haieracbridge.haierapi.ACDeviceManager;
@@ -19,7 +22,9 @@ import com.fastfend.haieracbridge.haierapi.ACDeviceManagerState;
 import com.fastfend.haieracbridge.haierapi.DeviceChecker;
 import com.fastfend.haieracbridge.haierapi.UserManager;
 import com.fastfend.haieracbridge.haierapi.webapi.WebApiResultStatus;
+import com.haier.uhome.usdk.api.uSDKErrorConst;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -53,6 +58,27 @@ public class BackgroundService extends Service
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.println(Log.INFO, "HACB", "Creating notification service...");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_SERVICE)
+                .setContentTitle("HaierACBridge")
+                .setContentText("Running")
+                .setContentIntent(pendingIntent)
+                .build();
+
+        Log.println(Log.INFO, "HACB", "Starting notification service...");
+        startForeground(1, notification);
+        //TODO: Add icons etc
+
+        try {
+            Process process = Runtime.getRuntime().exec("logcat > /sdcard/Download/log.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         Log.println(Log.INFO, "HACB", "Initializing service...");
         userManager.init(this.getApplicationContext());
@@ -67,28 +93,21 @@ public class BackgroundService extends Service
                 {
                     Log.println(Log.INFO, "HACB", "Starting SDK...");
                     deviceManager.start(authToken, list, value -> {
-                        Log.println(Log.INFO, "HACB", "SDK started...");
+                        if(value == uSDKErrorConst.RET_USDK_OK)
+                        {
+                            Log.println(Log.INFO, "HACB", "SDK started...");
 
-                        Timer deviceChecker = new Timer();
+                            Timer deviceChecker = new Timer();
 
-                        TimerTask checkDevices = new DeviceChecker(userManager, deviceManager, list);
-                        deviceChecker.scheduleAtFixedRate(checkDevices, 0, 5000);
-
+                            TimerTask checkDevices = new DeviceChecker(userManager, deviceManager, list);
+                            deviceChecker.scheduleAtFixedRate(checkDevices, 0, 5000);
+                        }
+                        else
+                        {
+                            stopSelf();
+                        }
                     });
-                    Log.println(Log.INFO, "HACB", "Creating notification service...");
-                    createNotificationChannel();
-                    Intent notificationIntent = new Intent(this, MainActivity.class);
-                    PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                            0, notificationIntent, 0);
-                    Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_SERVICE)
-                            .setContentTitle("HaierACBridge")
-                            .setContentText("Running")
-                            .setContentIntent(pendingIntent)
-                            .build();
 
-                    Log.println(Log.INFO, "HACB", "Starting notification service...");
-                    startForeground(1, notification);
-                    //TODO: Add icons etc
                 }
                 else
                 {
@@ -97,7 +116,7 @@ public class BackgroundService extends Service
             });
         }
 
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     private void createNotificationChannel() {
